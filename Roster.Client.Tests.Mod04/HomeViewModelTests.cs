96195a000410c21c7fe8d68667389b1449a4b7dc
Sidney Andrews <sidney@seesharprun.net>
2020-04-06 11:33:14 -0400

    Updated mod04 tests

--- Roster.Client.Tests.Mod04/HomeViewModelTests.cs
+++ Roster.Client.Tests.Mod04/HomeViewModelTests.cs
@@ -1,4 +1,5 @@
 using Roster.Client.ViewModels;
+using System.ComponentModel;
 using Xamarin.Forms.Mocks;
 using Xunit;
 
@@ -7,11 +8,21 @@ namespace Roster.Client.Tests.Mod04
     public class HomeViewModelTests
     {
         [Fact(DisplayName = "You need to create a public class named \"HomeViewModel\" in the \"Roster.Client.ViewModels\" namespace. @class-exists")]
-        public void ClassExistsTests()
+        public void ClassExistsTest()
         {
             MockForms.Init();
             HomeViewModel actual = new HomeViewModel();
             Assert.NotNull(actual);
+        }
+
+        [Fact(DisplayName = "Your \"HomeViewModel\" class should implement the \"System.ComponentModel.INotifyPropertyChanged\" interface. @class-implements-inotifypropertychanged-interface")]
+        public void ClassImplementsInterfaceTest()
+        {
+
+            MockForms.Init();
+            HomeViewModel actual = new HomeViewModel();
+            Assert.NotNull(actual);
+            Assert.IsAssignableFrom<INotifyPropertyChanged>(actual);
         }
     }
 }
