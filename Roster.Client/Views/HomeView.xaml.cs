99a34fec13ac35e92ee6e3432b3eade6973d18ff
vsummerill <victoria.summerill@causeway.com>
2023-03-08 12:13:00 +0000

    Module 2 completed

--- Roster.Client/Views/HomeView.xaml.cs
+++ Roster.Client/Views/HomeView.xaml.cs
@@ -8,6 +8,12 @@ namespace Roster.Client.Views
         public HomeView()
         {
             InitializeComponent();
+
+            BindingContext = new
+            {
+                Title = "Roster App"
+            };
+
         }
     }
 }
