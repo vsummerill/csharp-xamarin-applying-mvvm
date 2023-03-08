049654e5797fa2ceade0a0074e16683d9d26816e
vsummerill <victoria.summerill@causeway.com>
2023-03-08 15:09:58 +0000

    Module 4 complete

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -5,11 +5,30 @@ namespace Roster.Client.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
+        private string _title = "Roster App";
+
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string Title { get; set; } = "Roster App";
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("title"));
+            }
+        }
 
         public Command UpdateApplicationCommand { get; }
 
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(UpdateApplicationCommandExecute);
+        }
+
+        private void UpdateApplicationCommandExecute()
+        {
+            Title = "Roster App (v2.0)";
+        }
     }
 }
