eb896b49f67cf01e5aa398c9daf117a259ea48c5
vsummerill <victoria.summerill@causeway.com>
2023-03-08 15:32:08 +0000

    module 5

---
+++ Roster.Client/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Roster.Client.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+
+    }
+}
--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using Roster.Client.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace Roster.Client.ViewModels
@@ -21,9 +23,16 @@ namespace Roster.Client.ViewModels
 
         public Command UpdateApplicationCommand { get; }
 
+        public ObservableCollection<Person> People { get; }
+
         public HomeViewModel()
         {
             UpdateApplicationCommand = new Command(UpdateApplicationCommandExecute);
+            People = new ObservableCollection<Person>();
+            People.Add(new Person { Name = "Delores Feil", Company = "Legros Group" });
+            People.Add(new Person { Name = "Ann Zboncak", Company = "Ledner - Ferry" });
+            People.Add(new Person { Name = "Jaime Lesch", Company = "Herzog and Sons" });
+
         }
 
         private void UpdateApplicationCommandExecute()
@@ -32,3 +41,4 @@ namespace Roster.Client.ViewModels
         }
     }
 }
+
